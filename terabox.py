7054d3a84fa1a3a2e5afec2d54c4cf68ffd48e8d
Amit <heroku467@gmail.com>
2025-03-27 00:59:20 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -10,8 +10,7 @@ import os
 import time
 from status import format_progress_bar
 from video import download_video, upload_video
-from web import keep_alive
-
+from config import *
 load_dotenv('config.env', override=True)
 
 logging.basicConfig(level=logging.INFO)
