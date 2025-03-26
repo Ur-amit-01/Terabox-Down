f1836e9dab1cd99220276fd807f6b18940aec9d0
Amit <heroku467@gmail.com>
2025-03-27 00:58:07 +0530

    Update and rename config.env to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#All Variables Are Required
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TELEGRAM_API = os.getenv("TELEGRAM_API")
+TELEGRAM_HASH = os.getenv("TELEGRAM_HASH")
+FSUB_ID = os.getenv("FSUB_ID")
+DUMP_CHAT_ID = os.getenv("DUMP_CHAT_ID")
